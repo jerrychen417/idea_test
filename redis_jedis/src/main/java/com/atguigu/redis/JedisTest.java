82f60c4a0c1ea3319d8f26013aac8d7aab878c81
Jerrychen <19923351355@163.com>
2021-11-28 18:12:28 +0800

    first commit

---
+++ redis_jedis/src/main/java/com/atguigu/redis/JedisTest.java
@@ -0,0 +1,36 @@
+package com.atguigu.redis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class JedisTest {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("hadoop102", 6379);
+
+//        jedis.sadd("xiyouji","tangsanzhang","xunwukong","zhubajie");
+        jedis.expire("xiyouji",20);
+
+        Set<String> keys = jedis.keys("*");
+        for (String key : keys) {
+            System.out.println(key);
+        }
+
+        boolean b = true;
+        while (b) {
+            Long ttl = jedis.ttl("xiyouji");
+            if (ttl == -2) {
+                b=false;
+                continue;
+            }
+            System.out.println(ttl);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        jedis.close();
+    }
+}
